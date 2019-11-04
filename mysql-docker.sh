cb1e5bbc8278cc6ae262e15183ad862fc31b7dd0
Rafael Ferreira <r.ferreira@xgeeks.io>
2019-11-04 12:56:34 +0000

    fix(mysql-docker.sh): use variable to get the path of executing file instead hardcoded path

--- mysql-docker.sh
+++ mysql-docker.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
+SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
 docker run -d --name mysql -p 3306:3306 \
         -e MYSQL_ROOT_PASSWORD=ipl-mysql \
-        -v /data/development/ipl/tools/mysql/data:/var/lib/mysql \
-        -v /data/development/ipl/tools/mysql/conf:/etc/mysql/conf.d \
-        -v /data/development/ipl/tools/mysql/init:/docker-entrypoint-initdb.d \
+        -v $SCRIPTPATH/mysql/data:/var/lib/mysql \
+        -v $SCRIPTPATH/mysql/conf:/etc/mysql/conf.d \
+        -v $SCRIPTPATH/mysql/init:/docker-entrypoint-initdb.d \
         mysql
